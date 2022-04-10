ba0500df7393bfbfff3ce55d93e62d53f1c6bbb8
Pavan Kalyan <pavankalyanyadav321@gmail.com>
2022-04-10 23:52:44 +0530

    Ability to modify a contact in AB

--- Program.cs
+++ Program.cs
@@ -6,9 +6,9 @@ namespace AddressBooks
     {
         static void Main(string[] args)
         {
-
             Console.WriteLine("Welcome To Address Book System!");
             AddrBook.GetCustomer();
+            AddressBooks.AddrBook.Modify();
             AddrBook.ListingPeople();
 
         }
